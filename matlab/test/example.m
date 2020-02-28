7fc8b5c4efa063d0116214856b33c226d787ec04
Alexander Fyrdahl <alex@fyrdahl.io>
2020-02-28 12:43:38 +0100

    Update example.m

--- matlab/test/example.m
+++ matlab/test/example.m
@@ -7,9 +7,11 @@ data = permute(data,[1 2 4 3]);
 
 noise = noise(1:2:end-1) + 1i*noise(2:2:end);
 noise = reshape(noise,[512 26 128 2]);
-noise = permute(noise,[1 3 2]);
 
 traj = bart('traj -r -G -x256 -y420');
-traj_scaled = bart('scale 0.5', traj);
-grid = bart('nufft -i -t', traj_rad2, data);
+traj_scaled = bart('scale 0.6', traj);
+grid = bart('nufft -i -t', traj_scaled, data);
 img = bart('rss 8',grid);
+
+figure(1);imshow(abs(squeeze(noise(:,1,:,1))),[]);
+figure(2);imshow(fliplr(permute(brighten(log(1+abs(img)),0.8),[2 1])),[],'InitialMagnification',400);
